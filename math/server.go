1778cc073efaa62d520c42fd65181dfdf4152e8b
AdilahmedDev <80524770+AdilahmedDev@users.noreply.github.com>
2022-01-27 14:15:19 +0000

    track-1-part-13

--- math/server.go
+++ math/server.go
@@ -9,11 +9,22 @@ import (
 
 
 func MathServer(w http.ResponseWriter, r *http.Request) {
-	nums := r.URL.Query()["num"]
+	nums := []string{}
+	switch r.Header.Get("content-type"){
+		case "application/x-www-form-urlencoded":
+			r.ParseForm()
+			nums = r.Form["num"]
+	    case "application/json":
+
+		default:
+			nums = r.URL.Query()["num"]
+	}
 	formatter := methods.Formatter{}
 	adder := methods.NewAdder(map[int]int{}, formatter)
+
 	numbers := []int{}
 	numbers = adder.InputFromArgs(nums, numbers)
+
 	fmt.Fprint(w, formatter.FormatNumber(exercises.Add(numbers...)) )
 }
 
--- math/server_test.go
+++ math/server_test.go
@@ -3,13 +3,35 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
 func TestMathServer(t *testing.T) {
-	t.Run("returns Pepper's score", func(t *testing.T) {
+	t.Run("add numbers in url parameters", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/add?num=4&num=5&num=32", nil)
 		response := httptest.NewRecorder()
+
+		MathServer(response, request)
+
+		got := response.Body.String()
+		want := "41"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("add numbers in form encoded body", func(t *testing.T) {
+		data := url.Values{}
+		data.Add("num", "4")
+		data.Add("num", "5")
+		data.Add("num", "32")
+
+		response := httptest.NewRecorder()
+		request, _ := http.NewRequest(http.MethodPost, "/add", strings.NewReader(data.Encode()))
+		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 		MathServer(response, request)
 
