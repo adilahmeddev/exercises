b425cc65a83e3917444a8df27b15a18e9aafe0b1
AdilahmedDev <80524770+AdilahmedDev@users.noreply.github.com>
2022-01-27 16:26:50 +0000

    cleaning up

--- math/server.go
+++ math/server.go
@@ -10,9 +10,7 @@ import (
 	"strconv"
 )
 
-type numbers struct {
-	nums []int `json:"nums"`
-}
+
 func MathServer(w http.ResponseWriter, r *http.Request) {
 	nums := []string{}
 	defer r.Body.Close()
@@ -21,20 +19,8 @@ func MathServer(w http.ResponseWriter, r *http.Request) {
 			r.ParseForm()
 			nums = r.Form["num"]
 	    case "application/json":
-			var result map[string][]int
-			bytes, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				panic(err)
-			}
-			if err = json.Unmarshal(bytes, &result); err != nil {
-				panic(err)
-			}
-			fmt.Println(result)
-			for _, num := range result["nums"]{
-				nums = append(nums, strconv.Itoa(num))
-			}
-			fmt.Println(nums)
-	default:
+			nums = parseJSON(r, nums)
+		default:
 			nums = r.URL.Query()["num"]
 	}
 	formatter := methods.Formatter{}
@@ -43,7 +29,22 @@ func MathServer(w http.ResponseWriter, r *http.Request) {
 	numbers := []int{}
 	numbers = adder.InputFromArgs(nums, numbers)
 
-	fmt.Fprint(w, formatter.FormatNumber(exercises.Add(numbers...)) )
+	fmt.Fprint(w, formatter.FormatNumber(exercises.Add(numbers...)))
+}
+
+func parseJSON(r *http.Request, nums []string) []string {
+	var result map[string][]int
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	if err = json.Unmarshal(bytes, &result); err != nil {
+		panic(err)
+	}
+	for _, num := range result["nums"] {
+		nums = append(nums, strconv.Itoa(num))
+	}
+	return nums
 }
 
 
